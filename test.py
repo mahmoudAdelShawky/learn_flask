23f86969fed92197f3e5f5d72b893973cdb2f0e4
mahmoudAdelShawky <mahmoudashawky197@gmail.com>
2024-09-18 19:32:21 +0300

    css_update

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ skills_app = Flask(__name__)
 
 @skills_app.route("/")
 def home():
-    return render_template("home.html", pagetitle= "homePage", stylecss = "main.css")
+    return render_template("home.html", pagetitle = "home page", custom_css = "master" )
 
 @skills_app.route("/about")
 def about():
-    return render_template("about.html" , pagetitle = "aboutPage", stylecss="master.css" )
+    return render_template("about.html" , pagetitle = "aboutPage", custom_css = "main")
 
 if __name__ == "__main__":
     skills_app.run(debug= True )
